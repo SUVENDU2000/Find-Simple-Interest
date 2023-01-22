ddbf99021e7c4ad7f11c7eb07dd5c68b0a6f13b2
SUVENDU2000 <100366327+SUVENDU2000@users.noreply.github.com>
2023-01-22 15:37:39 +0530

    Add files via upload

---
+++ simpleInterest.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Finding Simple interest
+
+var totalPrice = 24000.0
+var time = 3.0
+var RoI = 8.5
+var SI = (totalPrice * time * RoI) / 100.0  // simple interest formula
+print("Amount: \(totalPrice)")
+print("Time: \(time)")
+print("Rate of Interest in % : \(RoI)")
+print("Simple interest is : \(SI)")
+
+
